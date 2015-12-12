a07a75842df47ec79b832a56c91c4ce57af7eda0
GrosPouletBiensGras <nico@coco.com>
2015-12-12 14:26:07 +0100

    Ajout sprite emitter

---
+++ Assets/Editor/SpriteEmitterInspector.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SpriteEmitter))]
+public class SpriteEmitterInspector : Editor {
+
+
+    int selected;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void OnInspectorGUI()
+    {
+        SpriteEmitter myTarget = (SpriteEmitter)target;
+
+        myTarget._spriteParticle = (SpriteParticle) EditorGUILayout.ObjectField("Particle :" , myTarget._spriteParticle, typeof(SpriteParticle), true );
+        myTarget._number = EditorGUILayout.IntField("Number of Particles :", myTarget._number);
+        myTarget._lifeTime = EditorGUILayout.FloatField("Lifetime :", myTarget._lifeTime);
+
+        string[] velocityOptions = new string[]{"Radial", "Random between 2 vectors"};
+        myTarget._velocityType = EditorGUILayout.Popup("Directions :", myTarget._velocityType, velocityOptions);
+
+        switch (myTarget._velocityType)
+        {
+            case (0):
+                myTarget._minSpeed = EditorGUILayout.FloatField(" Min speed : ", myTarget._minSpeed);
+                myTarget._maxSpeed = EditorGUILayout.FloatField(" Max speed : ", myTarget._maxSpeed);
+                break;
+            case (1):
+                myTarget._minVelocity = EditorGUILayout.Vector2Field(" Min velocity : ", myTarget._minVelocity);
+                myTarget._maxVelocity = EditorGUILayout.Vector2Field(" Max velocity : ", myTarget._maxVelocity);
+                break;
+        }
+       
+        
+
+        myTarget._playOnStart = EditorGUILayout.Toggle("Play on start :", myTarget._playOnStart);
+
+
+        //base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Scripts/SpriteParticle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+
+public class SpriteParticle : MonoBehaviour {
+
+    public Vector2 velocity = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += new Vector3(velocity.x * Time.deltaTime, velocity.y * Time.deltaTime, 0);
+	}
+}
