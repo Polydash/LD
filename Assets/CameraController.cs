0b464831613834ea58d29831765d6cdccf6af092
GrosPouletBiensGras <nico@coco.com>
2015-12-12 12:27:34 +0100

    Commit useless

---
+++ Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float _moveSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(_moveSpeed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(-_moveSpeed * Time.deltaTime, 0, 0);
+
+        }
+    }
+}
---
+++ Assets/VelocityScale.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VelocityScale : MonoBehaviour {
+
+    public float _scaleFactor = 1.0f;
+    public bool _useMaxScale = false;
+    public float _maxScale = 2.0f;
+
+    private Camera _myCamera;
+    private Vector3 _initialScale;
+    private float _scaleGoal;
+
+    // Use this for initialization
+    void Start () {
+        _myCamera = Camera.main;
+        _initialScale = this.transform.localScale;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        _scaleGoal = _initialScale.x + _myCamera.velocity.magnitude * _scaleFactor;
+        //Debug.Log("Camera velocity = " + _myCamera.velocity.magnitude);
+
+        this.transform.localScale = new Vector3(Mathf.Lerp(this.transform.localScale.x, _scaleGoal, 0.3f), _initialScale.y, _initialScale.z);
+    }
+}
