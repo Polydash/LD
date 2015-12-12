e31ff150f9c9f7d2d7fc39190b705461b818be68
Polydash <polydash.dev@gmail.com>
2015-12-12 23:34:06 +0100

    Added MovableMagnet

--- Assets/Scripts/LerpCamera.cs
+++ Assets/Scripts/LerpCamera.cs
@@ -6,9 +6,22 @@ public class LerpCamera : MonoBehaviour
 	public Transform _Player;
 	public float _Lerp;
 
+	public bool _Constrained;
+	public float _MinX;
+	public float _MaxX;
+	public float _MinY;
+	public float _MaxY;
+
 	private void Update()
 	{
 		transform.position += (_Player.position - transform.position) * _Lerp;
  		transform.position = new Vector3(transform.position.x, transform.position.y, -10.0f);
+
+		if(_Constrained)
+		{
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, _MinX, _MaxX),
+											Mathf.Clamp(transform.position.y, _MinY, _MaxY),
+											transform.position.z);
+		}
 	}
 }
---
+++ Assets/Scripts/MovableMagnet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableMagnet : MonoBehaviour
+{
+	private float _Distance;
+
+	public float _Speed;
+	public Vector3 _Position1;
+	public Vector3 _Position2;
+
+	private void Start()
+	{
+		_Distance = Vector3.Distance(_Position1, _Position2);
+	}
+
+	private void Update()
+	{
+		float ratio = Mathf.Cos(Time.time * _Speed / _Distance);
+		ratio = (ratio + 1.0f) * 0.5f;
+		transform.position = ratio * _Position1 + (1.0f - ratio) * _Position2;
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(_Position1, 0.1f);
+		Gizmos.DrawWireSphere(_Position2, 0.1f);
+	}
+}
