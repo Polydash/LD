6538998f7cc9747de9b6406c5e8b9f8a6bf6b48b
GrosPouletBiensGras <nico@coco.com>
2015-12-13 13:22:14 +0100

    Ajout particules

---
+++ Assets/rotateAround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateAround : MonoBehaviour {
+
+
+    public float _maxX, _maxY, _speed;
+    private Vector3 _initialPosition;
+
+	// Use this for initialization
+	void Start () {
+        _initialPosition = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Cos : " + Mathf.Cos(Time.time) + "  Sin : " + Mathf.Sin(Time.time));
+        this.transform.position = _initialPosition + new Vector3( Mathf.Cos(Time.time * _speed) * _maxX, Mathf.Sin(Time.time * _speed) * _maxY , 0) ;
+	}
+}
