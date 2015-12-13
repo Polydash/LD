fbf8a80b5af896eccaec113c999b76473d9baa95
Polydash <polydash.dev@gmail.com>
2015-12-13 17:22:01 +0100

    Level cleaner update

---
+++ Assets/Scripts/SizeScaleMat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SizeScaleMat : MonoBehaviour
+{
+	private void Awake()
+	{
+		for(int i=0; i<transform.childCount; ++i)
+		{
+			Transform child = transform.GetChild(i).transform;
+			float tileX = child.localScale.x;
+			float tileY = child.localScale.y;
+			child.GetComponent<Renderer>().material.SetTextureScale("_MainTex", new Vector2(tileX, tileY));
+		}
+	}
+}
