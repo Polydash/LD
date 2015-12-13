f23b25917d6a346dc736348775b6673c8011ad45
Polydash <polydash.dev@gmail.com>
2015-12-13 17:03:57 +0100

    Level cleaner integration

---
+++ Assets/LevelCleaner/SizeScaleMat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SizeScaleMat : MonoBehaviour
+{
+	private void Awake()
+	{
+		float tileX = transform.localScale.x;
+		float tileY = transform.localScale.y;
+		GetComponent<Renderer>().material.SetTextureScale("_MainTex", new Vector2(tileX, tileY));
+	}
+}
---
+++ Assets/Scripts/QuadInstancer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadInstancer : MonoBehaviour
+{
+	public GameObject _QuadPrefab;
+
+	private void Awake()
+	{
+		transform.position = Vector3.zero;
+		SpriteRenderer[] sprites = GameObject.FindObjectsOfType<SpriteRenderer>();
+
+		for(int i=0; i<sprites.Length; ++i)
+		{
+			GameObject quad = Instantiate<GameObject>(_QuadPrefab);
+			quad.transform.position = sprites[i].transform.position;
+			quad.transform.rotation = sprites[i].transform.rotation;
+			quad.transform.localScale = sprites[i].transform.localScale;
+
+			quad.transform.parent = transform.GetChild(0).transform;
+		}
+	}
+
+
+}
