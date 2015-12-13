3fc31e7318228db82f34778b608febd4e6a36797
GrosPouletBiensGras <nico@coco.com>
2015-12-13 17:45:32 +0100

    MajMenu

--- Assets/Scripts/CameraMenuScript.cs
+++ Assets/Scripts/CameraMenuScript.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class CameraLerp : MonoBehaviour {
+public class CameraMenuScript : MonoBehaviour {
 
     public Vector3 _target;
     public float _lerpSpeed = 1.0f, _threshold = 0.01f;
+    public List<Vector3> _positionList;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +19,10 @@ public class CameraLerp : MonoBehaviour {
             this.transform.position = Vector3.Lerp(this.transform.position, _target, _lerpSpeed);
         else
             this.transform.position = _target;
+    }
+
+    public void goTo(int index)
+    {
+        _target = _positionList[index];
     }
 }
