8bd166e175057e88fbaea71b84ef4aba0ae2c0fb
GrosPouletBiensGras <nico@coco.com>
2015-12-13 16:40:21 +0100

    Ajout de la torche de merde de Greg ce fils de pute

---
+++ Assets/Scripts/FireLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Light))]
+public class FireLight : MonoBehaviour {
+
+    public AnimationCurve _intensityVariation;
+    public float _factor = 1;
+
+    private Light _light;
+	// Use this for initialization
+	void Start () {
+        _light = this.GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _light.intensity = _intensityVariation.Evaluate( Time.time )* _factor;
+    }
+}
